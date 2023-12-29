989396bfb07b4ebe1d694ff9aecaf87d60fccafd
Josip <josip.opacic@gmail.com>
2023-12-29 17:09:26 +0100

    Add more properties to CryptoCurrencyCard.vue

--- src/components/CryptoCurrencyCard.vue
+++ src/components/CryptoCurrencyCard.vue
@@ -16,6 +16,15 @@ defineProps({
       <h3 class="text-lg font-semibold">
         ${{ ticker.quote.USD.price.toFixed(2) }}
       </h3>
+      <p>
+        Volume: ${{ ticker.quote.USD.volume_24h.toFixed(2) }}
+      </p>
+      <p>
+        Market Cap: ${{ ticker.quote.USD.market_cap.toFixed(2) }}
+      </p>
+      <p>
+        Circulating Supply: {{ ticker.circulating_supply }}
+      </p>
       <Badge severity="success" :value="ticker.quote.USD.percent_change_24h"></Badge>
     </template>
   </Card>
